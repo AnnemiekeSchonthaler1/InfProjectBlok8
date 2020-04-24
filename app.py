e766deaf6671b5bb47ac3a429bc17726a4c328ef
Sannevastaveren <43061946+Sannevastaveren@users.noreply.github.com>
2020-04-24 12:17:17 +0200

    updated to make results appear in html template

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
+import platform
+import pubmed_search
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    return render_template("Mainpage.html")
+    gene_dic = {}
+    print(platform.sys.version)
+    return render_template("Mainpage.html", genedic=gene_dic)
 
 
 @app.route('/result', methods=['POST', 'GET'])
@@ -13,6 +17,9 @@ def results():
     pub_date = ""
     disease_char = ""
     gene_list = []  # miss anders opslaan ligt aan hoe we dit later gaan gebruiken
+    mail = ""
+    gene_dic = {}
+
     if request.method == 'POST':
         result = request.form
         for key, value in result.items():
@@ -20,6 +27,8 @@ def results():
                 pub_date = value
             elif key == "disease_characteristic":
                 disease_char = value
+            elif key == "mail":
+                mail = value
             elif key == "gene_list":
                 values = value.split("\n")
                 for thing in values:
@@ -27,10 +36,15 @@ def results():
                     thing = thing.strip(" ")
                     if thing != " " and thing != "":
                         gene_list.append(thing)
-    print(pub_date)
-    print(disease_char)
-    print(gene_list)
-    return render_template("Mainpage.html")
+    pubmed_search.main(searchTerm=disease_char, geneList=gene_list)
+    for gene in gene_list:
+        gene_dic.update({gene: []})
+        for item in pubmed_search.pubmedEntry.instancesList:
+            if gene in item.getAbout():
+                gene_dic[gene].append(item)
+
+    print(gene_dic)
+    return render_template("Mainpage.html", genedic=gene_dic)
 
 
 if __name__ == '__main__':
