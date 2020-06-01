f8c27ee1137ae2892c66809f2c0bd3299fa22829
AnnemiekeSchonthaler1 <43265496+AnnemiekeSchonthaler1@users.noreply.github.com>
2020-06-01 16:36:34 +0200

    almost

--- Pubmed.py
+++ Pubmed.py
@@ -324,7 +324,7 @@ def calculateScores(termsList, accessionDict, pubmedInstance):
 
     maxMonthsAgo = (today.year - maxdateFormatted.year) * 12 + (today.month - maxdateFormatted.month)
 
-    score = ((voorkomensTermen/alleTermenVoorkomens) + (len(termsList)/len(alleTermen)) + 1)/(monthsAgo/maxMonthsAgo+1)
+    score = ((voorkomensTermen/alleTermenVoorkomens+1) + (len(termsList)/len(alleTermen)+1))/((monthsAgo/maxMonthsAgo)+1)
     print("score: "+str(score))
     # for key, value in pubmedEntry.instancesDict.items():
     #     id = key
@@ -471,3 +471,4 @@ class pubmedEntry:
 
     def getScore(self):
         return self.__score
+
