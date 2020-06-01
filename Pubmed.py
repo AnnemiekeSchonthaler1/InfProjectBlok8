4f6d85c66d2316c168961571c453f4d79c73cf70
AnnemiekeSchonthaler1 <43265496+AnnemiekeSchonthaler1@users.noreply.github.com>
2020-06-01 16:39:49 +0200

    haakjes

--- Pubmed.py
+++ Pubmed.py
@@ -324,7 +324,7 @@ def calculateScores(termsList, accessionDict, pubmedInstance):
 
     maxMonthsAgo = (today.year - maxdateFormatted.year) * 12 + (today.month - maxdateFormatted.month)
 
-    score = ((voorkomensTermen/alleTermenVoorkomens+1) + (len(termsList)/len(alleTermen)+1))/((monthsAgo/maxMonthsAgo)+1)
+    score = (((voorkomensTermen/alleTermenVoorkomens)+1) + ((len(termsList)/len(alleTermen))+1))/((monthsAgo/maxMonthsAgo)+1)
     print("score: "+str(score))
     # for key, value in pubmedEntry.instancesDict.items():
     #     id = key
